fcd7653bb486960e4c000af16123587b9838b2b1
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-02-11 20:13:30 -0500

    Removed most HTML and created basic header

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var helloWorld = 'Welcome to the Road to learn React';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{helloWorld}</h2>
     </div>
   );
 }
