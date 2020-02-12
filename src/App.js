fe758bdae0c0df95fd2a3c0de8947675e9cdf858
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-02-11 20:19:18 -0500

    Added Hot module reloading to index.js and changed the header in App.js to include an exclamation mark

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var helloWorld = 'Welcome to the Road to learn React';
+  const helloWorld = 'Welcome to the Road to learn React!';
   return (
     <div className="App">
       <h2>{helloWorld}</h2>
