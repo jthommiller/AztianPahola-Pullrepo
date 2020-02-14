8d30973a19e6a11f2e27c763f3f6c17c9da289e5
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-02-14 11:46:49 -0500

    converted the function App into a class in order to follow along with the book, which already had it as a class. THis may be due to me being on a later version of React.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
 const list = [
   {
-    title:'React',
+    title: 'React',
     url: 'https://reactjs.org',
     author: 'Jordan Walke',
     num_comments: 3,
@@ -12,31 +12,35 @@ const list = [
     objectID: 0,
   },
   {
-    title: 'Redux', 
-    url: 'https://redux.js.org/', 
-    author: 'Dan Abramov, Andrew Clark', 
-    num_comments: 2, 
-    points: 5, 
-    objectID: 1, 
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
 ];
 
-function App() {
-  //const helloWorld = 'Welcome to the Road to learn React!';
-  return (
-    <div className="App">
-      {list.map(item => 
-        <div key={item.objectID}>
-          <span>
-          <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
+class App extends Component {
+
+  constructor(props) { super(props); }
+
+  render() {
+    return (
+      <div className="App">
+        {list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
           </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
