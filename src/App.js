fca2f22822c82ee74b6bd38808f785bf450e8f19
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-02-16 18:56:46 -0500

    Delagated the search and table functionality to their own classes rather than all being in the App class.

--- src/App.js
+++ src/App.js
@@ -51,30 +51,57 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text"
-            onChange={this.onSearchChange}>
-          </input>
-        </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button">
-                Dismiss
+        <Search
+          onChange={this.onSearchChange}
+          value={searchTerm} />
+
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss} />
+      </div >
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          onChange={onChange}
+          value={value}>
+        </input>
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list,pattern,onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <button
+              onClick={() => this.onDismiss(item.objectID)}
+              type="button">
+              Dismiss
               </button>
-            </span>
-          </div>
-        )}
+          </span>
+        </div>
+        )}   
       </div>
     );
   }
