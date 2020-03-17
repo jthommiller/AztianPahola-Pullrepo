a7b13bc4f9bbe37db4038f42e5b15202ff1eb5ba
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-03-17 17:02:55 -0400

    Added error handling in case of erroneous requests

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -43,7 +44,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   setSearchTopStories(result) {
@@ -103,9 +104,13 @@ class App extends Component {
 
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page = (results && results[searchKey] && results[searchKey].page) || 0;
     const list = (results && results[searchKey] && results[searchKey].hits) || [];
+
+    if (error) {
+      return <p>Something went wrong.</p>
+    }
 
     return (
       <div className="page">
@@ -117,9 +122,14 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          onDismiss={this.onDismiss} />
+        {error ?
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div> :
+          <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />}
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
