9a6d5573d33ac152851393bf77895639f39c1864
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-03-30 17:21:43 -0400

    Tried installing and configuring jest, however I may need to redo it so this commit is more of a just in case thing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
+import {mount} from 'enzyme';
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+      expect(true).toEqual(true);
+    });
+  });
+
+
+
+
+
+
+
+
+
