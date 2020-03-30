fc8a28d694d00c3b98045602a74625950dce926e
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-03-30 17:36:26 -0400

    Ran npm install again to get appropriate version of Jest, tests seem to be working now

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,9 @@ import renderer from 'react-test-renderer';
 import App from './App';
 import {mount} from 'enzyme';
 
+// Blanket test to make sure testing is working in general
 describe('My Test Suite', () => {
     it('My Test Case', () => {
       expect(true).toEqual(true);
     });
   });
-
-
-
-
-
-
-
-
-
