678f47a03490c843bc2759121ce08e320c3ccdc8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 19:09:37 +0000

    Deploy jthommiller/AztianPahola-Pullrepo to github.com/jthommiller/AztianPahola-Pullrepo.git:gh-pages

---
+++ precache-manifest.b116e710399bbb508b0d23b64673e47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d247d68ee0d4cc6d673ab4683eda28",
+    "url": "/Learning-React/index.html"
+  },
+  {
+    "revision": "a2e27bdd6c6b3e1131cd",
+    "url": "/Learning-React/static/css/main.1c03a0e6.chunk.css"
+  },
+  {
+    "revision": "4cd5e257fae04ed23f1e",
+    "url": "/Learning-React/static/js/2.290b5f04.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/Learning-React/static/js/2.290b5f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e27bdd6c6b3e1131cd",
+    "url": "/Learning-React/static/js/main.6974c60c.chunk.js"
+  },
+  {
+    "revision": "e7b9a9b95824eca3ebca",
+    "url": "/Learning-React/static/js/runtime-main.db5802f1.js"
+  }
+]);
